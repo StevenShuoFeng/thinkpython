6be343062b00b73a327d1037300757bcfcf16e66
Shuo Feng <shuo@focusmotion.io>
2017-04-26 13:42:20 -0700

    file in&out sed

---
+++ chapter14.py
@@ -0,0 +1,38 @@
+import os
+from os.path import join, getsize
+
+# for root, dirs, files in os.walk('/Users/Steven/Desktop'):
+#     print root, "consumes",
+#     print sum(getsize(join(root, name)) for name in files),
+#     print "bytes in", len(files), "non-directory files"
+#     if 'CVS' in dirs:
+#         dirs.remove('CVS')  # don't visit CVS directories
+
+
+for root, dirs, files in os.walk('/Users/Steven/Desktop/ebooks papars'):
+	print files
+
+"""	
+Exercise 14.2. Write a function called sed that takes as arguments a pattern string,
+ a replacement string, and two filenames; it should read the first file and write the 
+ contents into the second file (creating it if necessary). If the pattern string appears anywhere in the file,
+  it should be replaced with the replacement string.
+"""
+
+def sed(pattern, replacestr, filein, fileout):
+	try:
+		fin = open(filein, 'r')
+		fout = open(fileout, 'w')
+
+		for line in fin:
+			line = line.replace(pattern, replacestr)
+			fout.write(line)
+
+		fin.close()
+		fout.close()
+
+	except:
+		print "Something went wrong"
+
+
+sed('gif', 'pdf', 'chap14in.txt', 'chap14out.txt')
